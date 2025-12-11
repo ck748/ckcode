be41743ae6d0ea720c8010f7aaf2b8b55b74b063
ck748 <3416585025@qq.com>
2025-12-11 21:09:54 +0800

    正确联盟链

--- defectDetection/defectDetection/src/main/java/com/ggbond/defectdetection/mapper/CheckMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ggbond.defectdetection.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ggbond.defectdetection.pojo.Check;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CheckMapper extends BaseMapper<Check> {
-}
--- defectDetection/defectDetection/src/main/java/com/ggbond/defectdetection/service/CheckService.java
+++
@@ -1,9 +0,0 @@
-package com.ggbond.defectdetection.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ggbond.defectdetection.pojo.Check;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CheckService extends IService<Check> {
-}
